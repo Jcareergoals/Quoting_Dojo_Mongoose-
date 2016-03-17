590745bdcb6a53d8616113a5e675663351ce4899
Jose Chery <Jcareergoals@hotmail.com>
2016-03-16 23:34:29 -0400

    added routes folder w/ routes file to handle all routes

--- server.js
+++ server.js
@@ -21,41 +21,7 @@ var quoteSchema = new mongoose.Schema({
 mongoose.model('Quote', quoteSchema); 
 var Quote = mongoose.model('Quote'); 
 
-app.get('/', function(req, res){
-	res.render('index');
-}); 
-app.get('/main', function(req, res){
-	Quote.find({}, function(err, users){
-		if(err){
-			console.log(err);
-			res.redirect('/');
-		} else {
-			res.render('main', {users:users});
-		}
-	});
-}); 
-app.post('/quotes', function(req, res){
-	if(!req.body.name){
-		console.log("Some fields were left empty!"); 
-		res.redirect('/');
-	} else {
-		var userQuote = new Quote({
-			name: req.body.name, 
-			quote: req.body.quote, 
-			date: new Date
-		}); 
-		userQuote.save(function(err){
-			if(err){
-				console.log(err);
-			} else {
-				console.log("Quote was successfully entered into the database!"); 
-				res.redirect('/main'); 
-			}
-		}); 
-	}
-		
-});
-
+var route = require('./routes/index.js')(app, Quote);
 
 app.listen(8888, function(){
 	console.log('Listening at port: 8888'); 
