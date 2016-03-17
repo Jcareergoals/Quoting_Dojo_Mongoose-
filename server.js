3dd1bbd8cdb09d3e55bf308600b1cade22e180f3
Jose Chery <Jcareergoals@hotmail.com>
2016-03-16 22:59:06 -0400

    added custom form validation for blank fields

--- server.js
+++ server.js
@@ -35,19 +35,25 @@ app.get('/main', function(req, res){
 	});
 }); 
 app.post('/quotes', function(req, res){
-	var userQuote = new Quote({
-		name: req.body.name, 
-		quote: req.body.quote, 
-		date: new Date
-	}); 
-	userQuote.save(function(err){
-		if(err){
-			console.log(err);
-		} else {
-			console.log("Quote was successfully entered into the database!"); 
-			res.redirect('/main'); 
-		}
-	}); 
+	if(!req.body.name){
+		console.log("Some fields were left empty!"); 
+		res.redirect('/');
+	} else {
+		var userQuote = new Quote({
+			name: req.body.name, 
+			quote: req.body.quote, 
+			date: new Date
+		}); 
+		userQuote.save(function(err){
+			if(err){
+				console.log(err);
+			} else {
+				console.log("Quote was successfully entered into the database!"); 
+				res.redirect('/main'); 
+			}
+		}); 
+	}
+		
 });
 
 
